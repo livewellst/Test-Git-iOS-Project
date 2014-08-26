de09448a5c92f02fa56d1553f86f0af11ed0bf44
Rodger Visitacion <rodgerv@Rodgers-MacBook-Pro.local>
2014-08-26 14:26:26 -0700

    added a test class

---
+++ CCTest.h
@@ -0,0 +1,13 @@
+//
+//  CCTest.h
+//  Git Test
+//
+//  Created by Rodger Visitacion on 8/26/14.
+//  Copyright (c) 2014 Test Coalition. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCTest : NSObject
+
+@end
---
+++ CCTest.m
@@ -0,0 +1,13 @@
+//
+//  CCTest.m
+//  Git Test
+//
+//  Created by Rodger Visitacion on 8/26/14.
+//  Copyright (c) 2014 Test Coalition. All rights reserved.
+//
+
+#import "CCTest.h"
+
+@implementation CCTest
+
+@end
